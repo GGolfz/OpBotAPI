4a6fbcbe0c876c2e121f80f976de4e74584955fd
GGolfz <wisarutgolf11@gmail.com>
2020-10-25 14:59:42 +0700

    [INSTALL] Firebase func

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const app = express();
 const port =process.env.PORT || 3000
@@ -8,16 +9,16 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.post('/api/opbot',(req,res)=>{
-    console.log(req);
-    const agent = new WebhookClient({ req, res });
+app.post('/api/opbot',functions.https.onRequest((request,response)=>{
+    console.log(request);
+    const agent = new WebhookClient({ request, response });
     const foodRandom = (agent) => {
         agent.add('Test food random')
     }
     let intentMap = new Map();
     interMap.set('Food Random',foodRandom)
     agent.handleRequest(intentMap);
-})
+}))
 app.listen(port, function() {
   console.log("Server started on port 3000");
 });
