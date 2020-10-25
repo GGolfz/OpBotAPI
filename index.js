c8d8f756fa8bc2ee2b591db011f998b361ef82da
GGolfz <wisarutgolf11@gmail.com>
2020-10-25 15:09:53 +0700

    [TEST]

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const app = express();
 const port =process.env.PORT || 3000
@@ -9,16 +8,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.post('/api/opbot',functions.https.onRequest((request,response)=>{
-    console.log(request);
-    const agent = new WebhookClient({ request, response });
-    const foodRandom = (agent) => {
-        agent.add('Test food random')
-    }
-    let intentMap = new Map();
-    interMap.set('Food Random',foodRandom)
-    agent.handleRequest(intentMap);
-}))
+app.post('/api/opbot', (request,response)=>{
+    console.log(request.data);
+    
+    // const agent = new WebhookClient({ request, response });
+    // const foodRandom = (agent) => {
+    //     agent.add('Test food random')
+    // }
+    // let intentMap = new Map();
+    // interMap.set('Food Random',foodRandom)
+    // agent.handleRequest(intentMap);
+})
 app.listen(port, function() {
   console.log("Server started on port 3000");
 });
