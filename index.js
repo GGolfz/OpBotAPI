b8ed24754893ee76d0c68c97f1de73796bf04e45
GGolfz <wisarutgolf11@gmail.com>
2020-10-25 14:46:26 +0700

    [ADD] Initial Code

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const {WebhookClient} = require('dialogflow-fulfillment');
+const app = express();
+const port =process.env.PORT || 3000
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.post('/api/opbot',(req,res)=>{
+    const agent = new WebhookClient({ req, res });
+    const foodRandom = (agent) => {
+        agent.add('Test food random')
+    }
+    let intentMap = new Map();
+    interMap.set('Food Random',foodRandom)
+    agent.handleRequest(intentMap);
+})
+app.listen(port, function() {
+  console.log("Server started on port 3000");
+});
