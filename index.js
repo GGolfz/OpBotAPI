ac2e65be9a42f77827a8b05beef9e31e33ccd472
GGolfz <wisarutgolf11@gmail.com>
2020-10-25 15:15:30 +0700

    [ADD] Food Random

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const {WebhookClient} = require('dialogflow-fulfillment');
 const app = express();
 const port =process.env.PORT || 3000
-app.set('view engine', 'ejs');
 
 app.use(bodyParser.json())
 app.get('/', (req, res) => {
@@ -12,16 +11,13 @@ app.get('/', (req, res) => {
     });
   })
 app.post('/api/opbot', (request,response)=>{
-    console.log('POST: /');
-  console.log('Body: ',request.body);
-    
-    // const agent = new WebhookClient({ request, response });
-    // const foodRandom = (agent) => {
-    //     agent.add('Test food random')
-    // }
-    // let intentMap = new Map();
-    // interMap.set('Food Random',foodRandom)
-    // agent.handleRequest(intentMap);
+    const agent = new WebhookClient({ request, response });
+    const foodRandom = (agent) => {
+        agent.add('Test food random')
+    }
+    let intentMap = new Map();
+    interMap.set('Food Random',foodRandom)
+    agent.handleRequest(intentMap);
 })
 app.listen(port, function() {
   console.log("Server started on port 3000");
