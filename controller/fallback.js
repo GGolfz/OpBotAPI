8979d75df988fe07c7bb1fbc988d9c6505126fdc
GGolfz <wisarutgolf11@gmail.com>
2020-10-25 23:22:46 +0700

    [ADD] Fat

--- controller/fallback.js
+++ controller/fallback.js
@@ -1,7 +1,5 @@
 const response = ["อะไรนะ ยัยตัวดี","พ้มไม่เข้าใจ","มันคือไรนะ"]
 exports.fallback = (agent) => {
-    console.log(agent)
-    console.log(Object.keys(agent))
     let answer = parseInt(Math.random() * 100) % 2;
     if(answer == 0){
         agent.add(response[parseInt(Math.random()*100)%response.length])
---
+++ controller/sendImage.js
@@ -0,0 +1,3 @@
+exports.fatgirl = (agent) => {
+    agent.add(new Image('https://ggolfz.github.io/bot-images/timetable.jpg'))
+}
