1c30d947d51ddb257a3f890c7b79c3cfb39e86a8
GGolfz <wisarutgolf11@gmail.com>
2020-10-30 08:46:05 +0700

    [ADD] DB

--- controller/fallback.js
+++ controller/fallback.js
@@ -1,5 +1,24 @@
+const admin = require('firebase-admin'); 
+var firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+};
+admin.initializeApp({firebaseConfig});
+var database = firebase.database()
+var dbRef = database.ref('/')
 const response = ["อะไรนะ ยัยตัวดี","พ้มไม่เข้าใจ","มันคือไรนะ"]
-exports.fallback = (agent) => {
+exports.fallback = async (agent) => {
+    await dbRef.equalTo(agent.queryText).on('child_added',(snapshot)=>{
+        let data =snapshot.val()
+        console.log(data)
+        agent.add(data[0].response)
+    })
     let answer = parseInt(Math.random() * 100) % 2;
     if(answer == 0){
         agent.add(response[parseInt(Math.random()*100)%response.length])
