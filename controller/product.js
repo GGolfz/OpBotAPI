b3fb7e1f84dc6d640874108a1e5355594fb1055a
GGolfz <wisarutgolf11@gmail.com>
2020-11-06 22:53:12 +0700

    [ADD] Card

--- controller/product.js
+++ controller/product.js
@@ -4,8 +4,10 @@ exports.product = async (agent) => {
   agent.add(
     new Card({
       title: 'Test Card',
+      imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
       text: `This is the body text of a card.`,
       buttonText: 'This is a button',
+      buttonUrl: 'https://assistant.google.com/'
     })
   )
   agent.add(new Suggestion('Test Suggestion'))
