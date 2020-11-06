6e59999a44fd70297bae7d81e1bb05c323ad4284
GGolfz <wisarutgolf11@gmail.com>
2020-11-06 22:51:37 +0700

    [ADD] Suggedtion

--- controller/product.js
+++ controller/product.js
@@ -1,5 +1,12 @@
-const {Card} = require('dialogflow-fulfillment')
+const { Card, Suggestion } = require('dialogflow-fulfillment')
 exports.product = async (agent) => {
-    agent.add('ต้องการรับสินค้าประเภทไหนคะ')
-    agent.add(new Card({title:'Test Card'}));
+  agent.add('ต้องการรับสินค้าประเภทไหนคะ')
+  agent.add(
+    new Card({
+      title: 'Test Card',
+      text: `This is the body text of a card.`,
+      buttonText: 'This is a button',
+    })
+  )
+  agent.add(new Suggestion('Test Suggestion'))
 }
