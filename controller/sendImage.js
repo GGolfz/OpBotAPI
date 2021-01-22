9fad84e742bea5b545ca4b17fc24e9840e60c65b
GGolfz <wisarutgolf11@gmail.com>
2021-01-22 18:17:34 +0700

    [ADD] Handle Fallback

--- controller/sendImage.js
+++ controller/sendImage.js
@@ -1,15 +1,43 @@
-const { Image } = require('dialogflow-fulfillment')
-const imgList = ["1.jpg","2.jpeg","3.JPG","4.JPG","5.JPG","6.JPG","7.JPG","8.JPG","9.JPG","10.JPG"]
+const { Image } = require("dialogflow-fulfillment");
+const imgList = [
+  "1.jpg",
+  "2.jpeg",
+  "3.JPG",
+  "4.JPG",
+  "5.JPG",
+  "6.JPG",
+  "7.JPG",
+  "8.JPG",
+  "9.JPG",
+  "10.JPG",
+];
 exports.fatgirl = (agent) => {
-    console.log('https://ggolfz.codes/opbot/image/'+imgList[parseInt(Math.random()*100)%imgList.length])
-    agent.add(new Image('https://ggolfz.codes/opbot/image/'+imgList[parseInt(Math.random()*100)%imgList.length]))
-}
+  console.log(
+    "https://ggolfz.codes/opbot/image/" +
+      imgList[parseInt(Math.random() * 100) % imgList.length]
+  );
+  agent.add(
+    new Image(
+      "https://ggolfz.codes/opbot/image/" +
+        imgList[parseInt(Math.random() * 100) % imgList.length]
+    )
+  );
+};
 exports.timeTable = (agent) => {
-    let year = agent.parameters.year
-    agent.add(new Image('https://ggolfz.codes/opbot/image/'+'y'+year+'.png'))
+  let year = agent.parameters.year;
+  if (year >= 1 && year <= 4) {
+    agent.add(
+      new Image("https://ggolfz.codes/opbot/image/" + "y" + year + ".png")
+    );
 
-    if(year == 1 || year == 2){
-        agent.add(new Image('https://ggolfz.codes/opbot/image/'+'y'+year+'ft'+'.png'))
-
+    if (year == 1 || year == 2) {
+      agent.add(
+        new Image(
+          "https://ggolfz.codes/opbot/image/" + "y" + year + "ft" + ".png"
+        )
+      );
     }
-}
+  } else {
+    agent.add("พ้มจะไปรู้ได้ยังไงนะ");
+  }
+};
