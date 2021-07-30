a95e1e7d96b99e3109d83923063c52a34087aa09
GGolfz <wisarutgolf11@gmail.com>
2021-07-30 14:51:51 +0700

    add: error handling :sparkles:

--- controller/morning.js
+++ controller/morning.js
@@ -10,7 +10,7 @@ exports.morning = async (req, res) => {
         subscriber.push(i);
       }
     });
-
+  console.log(subscriber);
   axios.post(
     "https://api.line.me/v2/bot/message/push",
     {
@@ -23,6 +23,6 @@ exports.morning = async (req, res) => {
         Authorization: `Bearer ${process.env.channel_access_token}`,
       },
     }
-  );
+  ).catch(err=> console.log(err));
   res.send({ success: true });
 };
