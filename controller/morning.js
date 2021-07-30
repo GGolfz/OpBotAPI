74de57536d7ccdd3b00cf589d48ec1f5c693b790
GGolfz <wisarutgolf11@gmail.com>
2021-07-30 14:55:26 +0700

    move: func to after loop :sparkles:

--- controller/morning.js
+++ controller/morning.js
@@ -6,23 +6,24 @@ exports.morning = async (req, res) => {
     .database()
     .ref("/subscriber")
     .on("value", (snapshot) => {
-      for (let i in snapshot.val()) {
+      let data = snapshot.val();
+      for (let i in data) {
         subscriber.push(i);
       }
+      console.log(subscriber);
+      await axios.post(
+        "https://api.line.me/v2/bot/message/push",
+        {
+          to: subscriber,
+          messages: [{ type: "text", text: 'Good Morning ค้าบเทอ' }],
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.channel_access_token}`,
+          },
+        }
+      ).catch(err=> console.log(err));
+      res.send({ success: true });
     });
-  console.log(subscriber);
-  axios.post(
-    "https://api.line.me/v2/bot/message/push",
-    {
-      to: subscriber,
-      messages: [{ type: "text", text: 'Good Morning ค้าบเทอ' }],
-    },
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.channel_access_token}`,
-      },
-    }
-  ).catch(err=> console.log(err));
-  res.send({ success: true });
 };
