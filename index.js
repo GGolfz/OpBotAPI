2cbd08ea2ccc60233f1f7e5eabed2364d98d307f
GGolfz <wisarutgolf11@gmail.com>
2021-08-29 22:01:01 +0700

    add: log :sparkles:

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.post("/api/opbot", (request, response) => {
   try {
     const agent = new WebhookClient({ request, response });
     let intentMap = new Map();
+    console.log(agent);
     console.log(agent.originalRequest.payload.data.source.userId);
     intentMap.set("Food Random", foodRandom);
     intentMap.set("Function", (agent) =>
